cc15f51f92522856e013e729f644119ea3e606c0
JGoliszewski <45105582+jgoliszewski@users.noreply.github.com>
2020-03-29 14:59:25 +0200

    zadanie 72 python

--- zbior zadan cke/72 - python - wychodzi zle/zadanit72.py
+++ zbior zadan cke/72 - python - wychodzi zle/zadanit72.py
@@ -12,6 +12,15 @@ zad2 = []
 #3
 zad3 = []
 zad3_max = 0
+def koncowka(a,b):
+	d = 0
+	for i in range(1,len(min(a,b))+1):
+		if a[-i] == b[-i]:
+			d += 1
+		else:
+			return d
+	return d
+
 for x in dane:
 	a, b = x
 	if len(b) > len(a):
@@ -23,23 +32,21 @@ for x in dane:
 	if a.find(b) == 0:
 		zad2.append([x,a[len(b):]])
 
-	dl_zakonczenia = 0
-	for i in range(1,len(b)):
-		if a[-i] == b[-i]:
-			dl_zakonczenia += 1
-		elif dl_zakonczenia > zad3_max:
-			zad3_max = dl_zakonczenia
-
+	d = koncowka(a, b)
+	if d > zad3_max:
+		zad3_max = d
 
 for x in dane:
 	a, b = x
-	if len(b) > len(a):
-		a, b = b, a
-	if len(a) >= len(b) * 3:
-		zad1 += 1
-	dl_zakonczenia = 0
-	for i in range(1,len(b)):
-		if a[-i] == b[-i]:
-			dl_zakonczenia += 1
-	if dl_zakonczenia == 16:
+	dl_zakonczenia = koncowka(a,b)
+	if dl_zakonczenia == 15:
 		zad3.append(x)
+
+with open('wyniki.txt','w') as w:
+	w.write('1)\n' + str(zad1) + '\n' + str(zad1_p)
+		+ '\n2)\n')
+	for x in zad2:
+		w.write(str(x) + '\n')
+	w.write('zad3\n')
+	for x in zad3:
+		w.write(str(x) + '\n')
